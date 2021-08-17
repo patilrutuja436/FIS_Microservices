1f79a0f9b0543b02bccfe867cbafcf012a1c64dc
Rutuja <patilrutuja436@gmail.com>
2021-08-17 11:07:09 +0530

    Implemented Emp-service from monolythic architecture

---
+++ emp-service/src/main/java/com/fis/empservice/dao/DepartmentDao.java
@@ -0,0 +1,24 @@
+package com.fis.empservice.dao;
+
+import java.util.ArrayList;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+import com.fis.empservice.SpringLearnConstants;
+import com.fis.empservice.bean.Department;
+@Component
+public class DepartmentDao {
+	
+	static ArrayList<Department> DEPARTMENT_LIST;
+	public ArrayList<Department> getAllDepartments(){
+		SpringLearnConstants.LOGGER.debug("Inside DepartmentDao Constructor");
+		ApplicationContext context = new ClassPathXmlApplicationContext("employee.xml");
+		DEPARTMENT_LIST = context.getBean("departmentList", ArrayList.class);
+		for (Department department : DEPARTMENT_LIST) {
+			SpringLearnConstants.LOGGER.debug(department.toString());
+		}
+		return DEPARTMENT_LIST;
+	}
+}
